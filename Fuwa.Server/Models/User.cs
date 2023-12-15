8d80cdc7ddbe22de791fa4fe98906aebdab484af
Hubert Kasperek <hubertkasp13@gmail.com>
2023-12-15 18:42:20 +0100

    Migrate to better project template

---
+++ Fuwa.Server/Models/User.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.Extensions.Hosting;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Fuwa.Models
+{
+    public class User
+    {
+        [Key]
+        [Required]
+        public string? Tag { get; set; }
+
+        [Required]
+        public string? Username { get; set; }
+
+        [Required]
+        // TODO: avatar
+        public string? Email { get; set; }
+
+        [Required]
+        public string? Password { get; set; }
+
+        [Required]
+        public Rank Rank { get; set; }
+
+        public string? Bio { get; set; }
+
+        public string? GithubProfile { get; set; }
+
+        public string? Company { get; set; }
+
+        public string? Location { get; set; }
+
+        public string? PersonalWebsite { get; set; }
+
+        [InverseProperty("Author")]
+        public ICollection<CodeSnippet> CodeSnippets { get; set; } = new List<CodeSnippet>();
+        [InverseProperty("Author")]
+        public ICollection<Post> Posts { get; set; } = new List<Post>();
+
+        [InverseProperty("Author")]
+        public ICollection<PostComment> PostComments { get; set; } = new List<PostComment>();
+
+        [InverseProperty("LikedBy")]
+        public ICollection<CodeSnippet> LikedSnippets { get; set; } = new List<CodeSnippet>();
+    }
+
+    public enum Rank
+    {
+        User,
+        Bot,
+        Admin
+    }
+}
---
+++ Fuwa.Server/Program.cs
@@ -0,0 +1,55 @@
+using Fuwa.Data;
+using Fuwa.Identity;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+
+builder.Services.AddDbContext<FuwaDbContext>(o => o.UseNpgsql(builder.Configuration.GetConnectionString("FuwaDB")));
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(options =>
+{
+    options.TokenValidationParameters = new Microsoft.IdentityModel.Tokens.TokenValidationParameters
+    {
+        ValidateIssuer = true,
+        ValidIssuer = JwtSettings.Issuer,
+        ValidateAudience = true,
+        ValidAudience = JwtSettings.Audience,
+        ValidateLifetime = true,
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(JwtSettings.Key)),
+        ValidateIssuerSigningKey = true,
+    };
+});
+
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+app.UseDefaultFiles();
+app.UseStaticFiles();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.MapFallbackToFile("/index.html");
+
+app.Run();
