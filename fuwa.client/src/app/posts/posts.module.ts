4b81309f25785b4b0c9fac1d034645cb339852ac
Hubert Kasperek <hubertkasp13@gmail.com>
2023-12-27 17:06:52 +0100

    Add timeAgo pipe to posts

--- fuwa.client/src/app/posts/posts.module.ts
+++ fuwa.client/src/app/posts/posts.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PostsComponent } from './posts/posts.component';
 import { RouterLink } from '@angular/router';
+import { TimeAgoPipe } from '../time-ago.pipe';
 
 
 
@@ -11,7 +12,8 @@ import { RouterLink } from '@angular/router';
   ],
   imports: [
     CommonModule,
-    RouterLink
+    RouterLink,
+    TimeAgoPipe
   ]
 })
 export class PostsModule { }
